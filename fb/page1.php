fb8713c06d0a87e159706d0c7068b2fe7d573e8e
halmsx <halmsx@gmail.com>
2014-06-02 02:15:10 +0800

    update fb module

--- fb/page1.php
+++ fb/page1.php
@@ -60,6 +60,33 @@ if ($user) {
       a:hover {
         text-decoration: underline;
       }
+
+      #fb-root .post {
+        padding: 4px;
+        margin-top: 10px;
+
+        border: 1px solid lightgray;
+        
+        max-width: 500px;
+      }
+      #fb-root .post .content {
+        font-size: 12px;
+      }
+      #fb-root .post .content img {
+        float: left; 
+        padding: 0px 5px 0px 0px;
+      }
+      #fb-root .post .bottom {
+        /*margin: 0 4px;*/
+        clear: both;
+      }
+      #fb-root .post #name img {
+        padding: 0px 6px 0px 0px;
+      }
+      #fb-root .post #name {
+        padding-bottom: 6px;
+        font-weight: bold;
+      }
     </style>
 
   </head>
@@ -78,18 +105,50 @@ if ($user) {
       <h3> Welcome <?php echo $user_profile['name']; ?> !!! </h3>
       <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
 
-      <div id="fb-root">
-      <!--<pre><?php print_r($user_response); ?></pre>-->
+      <div id="fb-root" style="margin-top: 20px">
+        <!--<pre><?php print_r($user_response); ?></pre>-->
         <?php
         foreach ($user_response['data'] as $i => $v) {
-          print ($v['from']['name']);
 
-          echo "<br>";
+          echo '<div id="' . $v['id'] . ' " class="post">';
+
+          print ('<div id="name"><a href="https://www.facebook.com/app_scoped_user_id/' . $v['from']['id'] . '" target="_blank"><img src="http://graph.facebook.com/' . $v['from']['id'] . '/picture" / style="vertical-align: top;">' . $v['from']['name'] . '</a></div>');
+
+          switch ($v['type']) {
+            case 'photo':
+              $photo = $v['picture'];
+              break;
+
+            case 'status':
+              break;
+
+            case 'video':
+              $photo = $v['picture'];
+              break;
+          }
+
+          echo '<div class="content">';
+
+          if (isset($v['picture']))
+            print ("<img src=\"$photo\" >");
+
+          if (isset($v['message']))
+            print (nl2br($v['message']) . '<br>');
+          else if (isset($v['story']))
+            print (nl2br($v['story']) . '<br>');
+
+          echo '</div>';
+
+          echo '<div class="bottom"></div>';
+
+          echo "</div>";
         }
         ?>
       <?php else: ?>
         <strong><em>You are not Connected.</em></strong>
       <?php endif ?>
     </div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   </body>
 </html>
